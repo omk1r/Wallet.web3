49fa272948da05b2fda9acb442a133d7105e7abd
Omkar <jadhavpatil2999@gmail.com>
2023-06-28 20:41:33 +0530

    Web3 wallet chrome extension

---
+++ mwallet/src/components/RecoverAccount.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RecoverAccount() {
+  return (
+    <>
+      <div className="content">recover account</div>
+    </>
+  );
+}
+
+export default RecoverAccount;
